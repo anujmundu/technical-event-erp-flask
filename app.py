426d67cd18727cfd75ca915308c3f271ccefb1bd
Anuj Mundu <anujmark.edwin.ame@gmail.com>
2026-02-28 03:09:15 +0530

    fix db init after init_app

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(BASE_DIR, "d
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
@@ -644,7 +646,6 @@ with app.app_context():
 
     db.session.commit()
 
-
-# Run locally only
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
